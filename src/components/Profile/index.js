6f11f674eed64ad8d3010d4ec3c6d7a4faf354e8
anelise300407@gmail.com <anelise300407@gmail.com>
2024-10-18 11:44:43 -0300

    estilização

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,16 @@
+import styles from "./styles.module.css"
 export default function Profile({foto, nome}){
     return(
         <>
-            <img src={foto} alt="Katherine Johnson" />
-            <p>{nome}</p>
+            <img 
+                src={
+                    foto ??
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdmbH5tdsn9PFu68Ibkg31vYCYzEBwuFHomQ&s"
+                } 
+                alt="Imagem"
+                className={styles.imagem}
+            />
+            <p>{nome ?? "Teste"}</p>
         </>
     );
 }
