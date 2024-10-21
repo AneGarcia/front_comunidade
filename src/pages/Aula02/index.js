4959ad9d0f50f626b45383ca14ee3e5b5fc3a78a
anelise300407@gmail.com <anelise300407@gmail.com>
2024-10-21 12:25:29 -0300

    form

---
+++ src/pages/Aula02/index.js
@@ -0,0 +1,43 @@
+import styles from "@/styles/Form.module.css"
+import { useState } from "react";
+
+export default function Form(){
+    const [title, setTitle] = useState("Título Inicial");
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const {nome, preco, descricao, foto} = e.target;
+        
+        alert(
+            "Valores recebidos: " + 
+            nome.value +
+            ", R$" +
+            preco.value +
+            ", " +
+            descricao.value
+        );
+    };
+    
+    return(
+        <div className={styles.container}>
+            <p>{title}</p>
+            <form onSubmit={handleSubmit} className={styles.formCadastro}>
+                <input type="text" id="nome" name="nome" placeholder="Nome"/>
+                <input type="number" id="preco" name="preco" placeholder="Preço"/>
+                <textarea type="text" id="descricao" name="descricao" placeholder="Descrição"/>
+                <input type="file" id="foto" name="foto"/>
+                <button type="submit">Adicionar</button>
+            </form>
+
+            <div className={styles.teste}>
+                <input 
+                    type="text" 
+                    id="titulo" 
+                    name="titulo" 
+                    placeholder="Titulo"
+                    onChange={(e) => setTitle(e.target.value) }/>
+            </div>
+        </div>
+    );
+}
