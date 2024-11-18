04a7cf5a1742d44f14453fce24c61c147cd83c5b
anelise300407@gmail.com <anelise300407@gmail.com>
2024-11-18 12:25:30 -0300

    excluir loja

--- src/components/cardLoja/index.js
+++ src/components/cardLoja/index.js
@@ -1,12 +1,24 @@
+import api from "@/services/api";
 import styles from "./styles.module.css";
+import {useRouter} from "next/router";
 
 export default function CardLoja({loja}) {
+    const router = useRouter();
+    const excluirLoja = (id) => {
+        api
+        .delete(`lojas/${id}`)
+        .then((res) => {
+            alert("Loja excluída com sucesso" );
+            router.reload();
+        })
+        .catch((err) => alert("Erro ao excluir"));
+    }
     return (
         <div className={styles.container}>
             <p>{loja.nome}</p>
             <span>{loja.endereco}</span>
             <button type="button" className={styles.btnEditar}>Editar</button>
-            <button type="button" className={styles.btnExcluir}>Excluir</button>
+            <button type="button" className={styles.btnExcluir} onClick={() => excluirLoja(loja.id)}>Excluir</button>
         </div>
     );
 }
